9c3e53611da551ccd400591489a07b27d5dade78
Brent De Roeck <brent.deroeck@marlon.be>
2020-06-13 21:35:59 +0200

    Added

--- client/express.js
+++ client/express.js
@@ -2,23 +2,19 @@ const express = require('express');
 const epf = require('express-php-fpm').default;
 const path = require('path');
 const app = express();
-const portNumber = 3000;
+const portNumber = 3500;
+const host = '127.0.0.1';
 const sourceDir = 'dist';
-const backendOptions = {
-    documentRoot: path.resolve(__dirname, sourceDir, '/backend'),
-    env: {},
-    socketOption: { port: portNumber }
-}
 
 app.use(express.static(sourceDir));
 
-app.get('*', (request, response) => {
+app.get('/', (request, response) => {
 	response.sendFile(path.resolve(__dirname, sourceDir, 'index.html'));
 });
 
-app.get('/login', epf(backendOptions));
+// app.get('/login';
 
-app.listen(portNumber, () => {
+app.listen(portNumber, host, () => {
 	console.log(`Express web server started: http://localhost:${portNumber}`);
 	console.log(`Serving content from /${sourceDir}/`);
 });
